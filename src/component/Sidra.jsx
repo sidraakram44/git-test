1675a7206c77883e00cad7a3af73b5a98d6b88c6
sidraakram <akramsidra44@gmail.com>
2025-03-12 03:12:54 -1200

    test

---
+++ src/component/Sidra.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidra = () => {
+  return (
+    <div>
+        <h1>Sidra Akram</h1>
+        Search: <input type="text" placeholder='search' />
+        <button>Add Me</button>
+    </div>
+  )
+}
+
+export default Sidra
