92b91d51adc11925a8dd006f4519c3393aa033a9
sidraakram <akramsidra44@gmail.com>
2025-03-12 03:28:58 -1200

    new test file

--- src/component/Sidra.jsx
+++ src/component/Sidra.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Sidra = () => {
   return (
     <div>
-        <h1>Sidra Akram</h1>
+        <h1>Sidra Akram live in pattoki</h1>
         Search: <input type="text" placeholder='search' />
         <button>Add Me</button>
     </div>
--- src/component/ZainWork.jsx
+++ src/component/ZainWork.jsx
@@ -4,6 +4,7 @@ export const ZainWork = () => {
   return (
     <div>ZainWork
         <h1 style={{backgroundColor:"red"}}>Muhammad Zain naseer</h1>
+       Search: <input type="text" placeholder='search' />
     </div>
   )
 }
