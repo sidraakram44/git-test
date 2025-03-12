1ee542e0e9341fb19902151f2396da5a53c85c81
mzain030 <113288311+mzain030@users.noreply.github.com>
2025-03-12 15:23:17 +0500

    create a file name zain work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidra from './component/Sidra';
+import { ZainWork } from './component/ZainWork';
 
 function App() {
   return (
     <div className="App">
     <Sidra/>
+    <ZainWork/>
     </div>
   );
 }
---
+++ src/component/ZainWork.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ZainWork = () => {
+  return (
+    <div>ZainWork
+        <h1 style={{backgroundColor:"red"}}>Muhammad Zain naseer</h1>
+    </div>
+  )
+}
