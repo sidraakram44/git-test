b265d4b29171e6e5a23a827da264d7eba9ccca85
mzain030 <113288311+mzain030@users.noreply.github.com>
2025-03-13 15:11:13 +0500

    alert

--- src/component/Sidra.jsx
+++ src/component/Sidra.jsx
@@ -6,6 +6,8 @@ const Sidra = () => {
         <h1 style={{backgroundColor:"gray"}}>Sidra Akram live in pattoki</h1>
         Search: <input style={{backgroundColor:"olive"}} type="text" placeholder='search' />
         <button>Add Me</button>
+        {alert("zain")}
+        <h2>Softwearengg</h2>
     </div>
   )
 }
