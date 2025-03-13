fb6fd44eb6ae2abb45a299f76936a0ae966a141b
sidraakram <akramsidra44@gmail.com>
2025-03-13 03:39:22 -1200

    new

--- src/component/Sidra.jsx
+++ src/component/Sidra.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Sidra = () => {
+
   return (
     <div>
         <h1 style={{backgroundColor:"gray"}}>Sidra Akram live in pattoki</h1>
         Search: <input style={{backgroundColor:"olive"}} type="text" placeholder='search' />
         <button>Add Me</button>
+        <h2 style={{color:"orange"}}>Arham Kalim</h2>
     </div>
   )
 }
